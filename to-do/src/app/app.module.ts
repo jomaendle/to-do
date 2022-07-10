dcae848a84f127878fd1fe923b1b666f4a7bc619
Johannes Maendle <johannes@studysmarter.de>
2022-07-10 13:04:13 +0200

    add localisation

--- to-do/src/app/app.module.ts
+++ to-do/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListModule } from './to-do-list/to-do-list.module';
 import { AddToDoItemModule } from './add-to-do-item/add-to-do-item.module';
+
+import '@angular/localize/init';
 
 @NgModule({
   declarations: [AppComponent],
