cf6887d2d2d9f7d87f571a67061b2672658db5c2
Johannes Maendle <johannes@studysmarter.de>
2022-07-15 09:18:32 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,18 +18,18 @@ if (!ATLAS_URI) {
 const app = express();
 const apiRoute = '/.netlify/functions/server/todos';
 
-app.use(cors());
-
-// Add routes
-app.use(apiRoute, toDoRouter);
-
-// start Express server
-app.listen(5200, () => {
-  console.log('Server started on port 5200!');
-});
-
 connectToDatabase(ATLAS_URI)
   .then(() => {
+    app.use(cors());
+
+    // Add routes
+    app.use(apiRoute, toDoRouter);
+
+    // start Express server
+    app.listen(5200, () => {
+      console.log('Server started on port 5200!');
+    });
+
     console.log('Connected to Database');
   })
   .catch((err) => {
